949c9f8eee16f62bb1e27c97c04f78c582478053
guptaji6489 <guptaji6489@gmail.com>
2020-07-02 00:44:57 +0530

    we have to find all possible path in given matrix from start to end

---
+++ count_board_path.cpp
@@ -0,0 +1,37 @@
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int board(int start,int n)
+{
+    //static int start = 0;
+    int c = 0;
+    if(start==n)
+    {
+
+        return 1;
+    }
+    if(start>n)
+    {
+        return 0;
+    }
+
+
+    for(int jump = 1;jump<=3;jump++)
+    {
+        //start =start+jump;
+        c += board(start+jump,n);
+    }
+    return c;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<board(0,n);
+}
+
+
+
