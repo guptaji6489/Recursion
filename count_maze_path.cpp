fbc7d6e25e9db7f734f3ee44c1cb24e1e34504b1
guptaji6489 <guptaji6489@gmail.com>
2020-07-02 18:25:01 +0530

    we have to find all possible maze path  from start to end

---
+++ count_maze_path.cpp
@@ -0,0 +1,34 @@
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int board(int sr,int sc,int er,int ec)
+{
+
+    if(sr==er and ec==sc)
+    {
+        return 1;
+    }
+    if(sr>er or sc>ec)
+    {
+        return 0;
+    }
+
+
+
+    int right = board(sr,sc+1,er,ec);
+    int down = board(sr+1,sc,er,ec);
+    int c = right + down;
+    return c;
+}
+
+int main()
+{
+
+    cout<<board(0,0,2,2);
+}
+
+
+
+
