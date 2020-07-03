614e1847a14ae08598e6ed213afe090d9547aeaf
guptaji6489 <guptaji6489@gmail.com>
2020-07-03 14:56:16 +0530

    count number of binary ones with no consicutive ones

---
+++ cout_binary_with_no_consi_ones.cpp
@@ -0,0 +1,27 @@
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int countb(int n)
+{
+
+    if(n==1 or n==2)
+    {
+        return n+1;
+    }
+            int sub = countb(n-1)+countb(n-2);
+            return sub;
+
+
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<countb(n);
+}
+
+
